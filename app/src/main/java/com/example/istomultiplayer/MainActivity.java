d42f30e07b9fe705072ae27fe60a29f106528fe6
Mayank <Kanakiyamayank182003@gmail.com>
2024-04-12 09:21:54 +0530

    12-04-2024
    1. Four player name send into MainActivity.
    2. Change all GridLayout into GridView.

--- app/src/main/java/com/example/istomultiplayer/MainActivity.java
+++ app/src/main/java/com/example/istomultiplayer/MainActivity.java
@@ -2,24 +2,40 @@ package com.example.istomultiplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.istomultiplayer.ImageAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    TextView getUname;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
-            GridView gridView = findViewById(R.id.gridView0);
+            GridView gridView = findViewById(R.id.gridView23);
             gridView.setAdapter(new ImageAdapter(this));
         } catch (Exception e) {
             Log.d("MyError", e.toString());
         }
+        //This below code get set username - start here
+        try {
+            getUname = findViewById(R.id.getUname);
+            Intent intent = getIntent();
+            String ReceivedPlayer1 = intent.getStringExtra("Player1");
+            Log.d("Player1", ReceivedPlayer1);
+            getUname.setText(ReceivedPlayer1);
+        }catch (Exception e){
+            Log.d("ReceviedUnameError",e.toString());
+        }
+        //This below code get set username - End here
     }
 }
--- app/src/main/java/com/example/istomultiplayer/UnameAdd.java
+++ app/src/main/java/com/example/istomultiplayer/UnameAdd.java
@@ -2,13 +2,44 @@ package com.example.istomultiplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UnameAdd extends AppCompatActivity {
+
+    EditText Player1EditText;
+    EditText Player2EditText;
+    EditText Player3EditText;
+    EditText Player4EditText;
+    Button startGameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uname_add);
+        Player1EditText = findViewById(R.id.Player1EditText);
+        Player2EditText = findViewById(R.id.Player2EditText);
+        Player3EditText = findViewById(R.id.Player3EditText);
+        Player4EditText = findViewById(R.id.Player4EditText);
+        startGameBtn = findViewById(R.id.startGameBtn);
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    String TextSendPlayer1 = Player1EditText.getText().toString();
+                    String TextSendPlayer2 = Player2EditText.getText().toString();
+                    String TextSendPlayer3 = Player3EditText.getText().toString();
+                    String TextSendPlayer4 = Player4EditText.getText().toString();
+                    Intent intent = new Intent(UnameAdd.this, MainActivity.class);
+                    intent.putExtra("Player1", TextSendPlayer1);
+                    intent.putExtra("Player2", TextSendPlayer2);
+                    intent.putExtra("Player3", TextSendPlayer3);
+                    intent.putExtra("Player4", TextSendPlayer4);
+                    startActivity(intent);
+            }
+        });
     }
 }
