f001c1950d9ef9bcbcacf283bcbafacabf8f15ac
Mayank <Kanakiyamayank182003@gmail.com>
2024-04-13 11:21:24 +0530

    13-04-2024
    Finding image name from the GridView code is ready.

--- app/src/main/java/com/example/istomultiplayer/ImageAdapter.java
+++ app/src/main/java/com/example/istomultiplayer/ImageAdapter.java
@@ -1,17 +1,23 @@
 package com.example.istomultiplayer;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private List<String> mImageNames;
 
     public ImageAdapter(Context context) {
         mContext = context;
+        mImageNames = new ArrayList<>();
     }
 
     @Override
@@ -47,54 +53,80 @@ public class ImageAdapter extends BaseAdapter {
         switch (position % 16) {
             case 0:
                 imageView.setImageResource(R.drawable.a1_h1_p1); // Yellow
+                addImageName("a1_h1_p1");
                 break;
             case 1:
                 imageView.setImageResource(R.drawable.a2_h1_p2); // Yellow
+                addImageName("a2_h1_p2");
                 break;
             case 2:
                 imageView.setImageResource(R.drawable.a3_h1_p3); // Yellow
+                addImageName("a3_h1_p3");
                 break;
             case 3:
                 imageView.setImageResource(R.drawable.a4_h1_p4); // Yellow
+                addImageName("a4_h1_p4");
                 break;
             case 4:
                 imageView.setImageResource(R.drawable.a1_h2_p1); // Green
+                addImageName("a1_h2_p1");
                 break;
             case 5:
                 imageView.setImageResource(R.drawable.a2_h2_p2); // Green
+                addImageName("a2_h2_p2");
                 break;
             case 6:
                 imageView.setImageResource(R.drawable.a3_h2_p3); // Green
+                addImageName("a3_h2_p3");
                 break;
             case 7:
                 imageView.setImageResource(R.drawable.a4_h2_p4); // Green
+                addImageName("a4_h2_p4");
                 break;
             case 8:
                 imageView.setImageResource(R.drawable.a1_h3_p1); // Blue
+                addImageName("a1_h3_p1");
                 break;
             case 9:
                 imageView.setImageResource(R.drawable.a2_h3_p2); // Blue
+                addImageName("a2_h3_p2");
                 break;
             case 10:
                 imageView.setImageResource(R.drawable.a3_h3_p3); // Blue
+                addImageName("a3_h3_p3");
                 break;
             case 11:
                 imageView.setImageResource(R.drawable.a4_h3_p4); // Blue
+                addImageName("a4_h3_p4");
                 break;
             case 12:
-                imageView.setImageResource(R.drawable.a1_h4_p1); // Blue
+                imageView.setImageResource(R.drawable.a1_h4_p1); // Red
+                addImageName("a1_h4_p1");
                 break;
             case 13:
-                imageView.setImageResource(R.drawable.a2_h4_p2); // Blue
+                imageView.setImageResource(R.drawable.a2_h4_p2); // Red
+                addImageName("a2_h4_p2");
                 break;
             case 14:
-                imageView.setImageResource(R.drawable.a3_h4_p3); // Blue
+                imageView.setImageResource(R.drawable.a3_h4_p3); // Red
+                addImageName("a3_h4_p3");
                 break;
             case 15:
-                imageView.setImageResource(R.drawable.a4_h4_p4); // Blue
+                imageView.setImageResource(R.drawable.a4_h4_p4); // Red
+                addImageName("a4_h4_p4");
                 break;
         }
-
         return imageView;
+    }
+    // Method to add image name to the list
+    private void addImageName(String imageName) {
+        if (!mImageNames.contains(imageName)) {
+            mImageNames.add(imageName);
+        }
+    }
+
+    // Method to get the list of image names
+    public List<String> getImageNames() {
+        return mImageNames;
     }
 }
--- app/src/main/java/com/example/istomultiplayer/MainActivity.java
+++ app/src/main/java/com/example/istomultiplayer/MainActivity.java
@@ -5,24 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.istomultiplayer.ImageAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     TextView getUname;
-    Button btn;
-
+    List<String> imageNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
             GridView gridView = findViewById(R.id.gridView23);
-            gridView.setAdapter(new ImageAdapter(this));
+            ImageAdapter imageAdapter = new ImageAdapter(this);
+            gridView.setAdapter(imageAdapter);
+
+            // Wait for the GridView to finish setting up its child views
+            gridView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    // Remove the listener to avoid multiple calls
+                    gridView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                    // Get the list of image names from the adapter
+                    imageNames = imageAdapter.getImageNames();
+                        for (String name : imageNames) {
+                            Log.d("Image Name", name);
+                    }
+                }
+            });
         } catch (Exception e) {
             Log.d("MyError", e.toString());
         }
@@ -33,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
             String ReceivedPlayer1 = intent.getStringExtra("Player1");
             Log.d("Player1", ReceivedPlayer1);
             getUname.setText(ReceivedPlayer1);
-        }catch (Exception e){
-            Log.d("ReceviedUnameError",e.toString());
+        } catch (Exception e) {
+            Log.d("ReceviedUnameError", e.toString());
         }
         //This below code get set username - End here
     }
